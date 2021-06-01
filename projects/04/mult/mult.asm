1fa726f7adfd754ab5e2dedf750cddcfb0829ecc
Kyle Glaws <kyle.glaws@gmail.com>
2021-06-01 15:25:46 -0400

    finish Mult

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,69 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// R0 is the 'adding' register, and R1 is the 'decrementing' register
+// so make sure R1 < R0
+(init)
+  // initialize R2 to zero
+  @2
+  M = 0
+
+  // copy R0 into R3
+  @0
+  D = M
+  @3
+  M = D
+
+  // compare R3 to R1
+  @1
+  D = M
+  @3
+  D = M - D
+
+  // jump to add_loop if R0 >= R1
+  @add_loop
+  D; JGE
+
+(swap)
+  // move R0 into R3
+  @0
+  D = M
+  @3
+  M = D
+
+  // move R1 into R0
+  @1
+  D = M
+  @0
+  M = D
+
+  // move R3 into R1
+  @3
+  D = M
+  @1
+  M = D
+
+(add_loop)
+  // check if R1 == 0
+  @1
+  D = M;
+  @done
+  D; JEQ
+
+  // set R2 = R2 + R0
+  @0
+  D = M
+  @2
+  M = M + D
+
+  // decrement R1
+  @1
+  M = M - 1
+
+  // loop
+  @add_loop
+  0; JEQ
+
+// infinite loop
+(done)
+  0; JEQ
